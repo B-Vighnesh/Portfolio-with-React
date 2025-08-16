7254c41aa2dbfd80186eb138d1c4a4398ddba90e
B Vighnesh Kumar <vighneshsheregar2004@gmail.com>
2025-08-16 14:56:03 +0530

    Fix index.html entrypoint for Vite

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/My-Portfolio/',   // 👈 Must match repo name exactly (case-sensitive!)
+  base: '/My-Portfolio/',   // 👈 MUST match repo name exactly
   plugins: [react()],
 })
