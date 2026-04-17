71feeff2b5f9e49c78cdcfc78d5700211d571f39
B Vighnesh Kumar <vighneshsheregar2004@gmail.com>
2026-04-17 13:27:10 +0530

    commited vite

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/-Portfolio-with-React/',   // 👈 MUST match repo name exactly
+  base: '/Portfolio-with-React/',   // 👈 MUST match repo name exactly
   plugins: [react()],
 })
